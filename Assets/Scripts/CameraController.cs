68eed424d917b44c0cb4a3aff5f87f70ee3ac8ee
SalvageMan <170870398+SalvageMan@users.noreply.github.com>
2025-08-13 13:09:15 -0500

    Set up camera that follows player and temporary stage BorderMarkers

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTrg;
+    public float smoothSpeed = 0.1f;
+    public Vector3 offSet;
+
+    [SerializeField] float minPosX, maxPosX;
+    [SerializeField] float minPosY, maxPosY;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+       Vector3 desiredPos = playerTrg.position + offSet;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = smoothPos;
+
+        //Clamp Camera to stage boundary
+        transform.position = new Vector3(Mathf.Clamp(smoothPos.x, minPosX, maxPosX), Mathf.Clamp(smoothPos.y, minPosY, maxPosY), transform.position.z);
+    }
+}
