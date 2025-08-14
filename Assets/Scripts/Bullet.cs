5f786c68eb130c62c1a66c5f44bdceed209eefb8
SalvageMan <170870398+SalvageMan@users.noreply.github.com>
2025-08-14 13:16:26 -0500

    updated bullet script

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
